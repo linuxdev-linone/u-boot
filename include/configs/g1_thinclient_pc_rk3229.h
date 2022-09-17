e8b7e09e7fb06ed3b37128c7316412c4c1085681
Linuxdev Linone <admin@linone.in>
2022-09-17 22:47:34 +0530

    board: rockchip: Add support for G1 Thinclient PC based on RK3229
    Signed-off-by: Linuxdev Linone <admin@linone.in>

---
+++ include/configs/g1_thinclient_pc_rk3229.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2017 Rockchip Electronics Co., Ltd
+ */
+
+#ifndef _G1_THINCLIENT_PCI_CONFIG_H
+#define _G1_THINCLIENT_PCI_CONFIG_H
+
+#include <configs/rk322x_common.h>
+
+#endif
